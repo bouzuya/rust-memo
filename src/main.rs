4cbcd4b4e54904bd4d36adac3f559b59261b91ec
bouzuya <m@bouzuya.net>
2020-08-07 23:38:33 +0900

    add server subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use actix_web::{web, HttpResponse};
 use chrono::prelude::*;
-use clap::{App, Arg, SubCommand};
+use clap;
 use std::fs::{read_to_string, File};
 use std::io::Write;
 
@@ -12,18 +13,46 @@ fn create_new_file(content: &str) -> Result<String, Box<dyn std::error::Error>> 
     Ok(file_name)
 }
 
+async fn index() -> impl actix_web::Responder {
+    HttpResponse::Found()
+        .header(actix_web::http::header::LOCATION, format!("/permalinks"))
+        .finish()
+}
+
+async fn permalinks() -> actix_web::Result<HttpResponse> {
+    let mut entries = std::fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path().to_str().unwrap().to_owned()))
+        .collect::<Result<Vec<String>, std::io::Error>>()?;
+    entries.sort();
+    let s = entries.join("\n");
+    Ok(HttpResponse::Ok().body(s))
+}
+
+#[actix_rt::main]
+async fn run_server() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .route("/", web::get().to(index))
+            .route("/permalinks", web::get().to(permalinks))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = App::new("memo")
+    let matches = clap::App::new("memo")
         .subcommand(
-            SubCommand::with_name("edit")
+            clap::SubCommand::with_name("edit")
                 .about("Creates a new memo that obsoletes the specified memo")
                 .arg(
-                    Arg::with_name("ID")
+                    clap::Arg::with_name("ID")
                         .help("the id of the memo to edit")
                         .required(true),
                 ),
         )
-        .subcommand(SubCommand::with_name("new").about("Creates a new memo"))
+        .subcommand(clap::SubCommand::with_name("new").about("Creates a new memo"))
+        .subcommand(clap::SubCommand::with_name("server").about("Runs server"))
         .get_matches();
     match matches.subcommand() {
         ("new", _) => {
@@ -40,6 +69,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let new_file_name = create_new_file(&content)?;
                 println!("{} -> {}", old_file_name, new_file_name);
             }
+        }
+        ("server", _) => {
+            run_server()?;
         }
         _ => {}
     }
