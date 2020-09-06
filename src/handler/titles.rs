b412fe3a19f436fe21dec2ece43a3a41c11a4fa8
bouzuya <m@bouzuya.net>
2020-09-06 10:25:45 +0900

    extract list_title use_case

--- src/handler/titles.rs
+++ src/handler/titles.rs
@@ -1,22 +1,19 @@
 use crate::handler_helpers::is_all;
-use crate::helpers::{is_obsoleted, read_obsoleted_map, read_title_map};
 use crate::template::{TitlesItemTemplate, TitlesTemplate};
 use crate::url_helpers::{title_url, titles_url};
 use actix_web::HttpResponse;
 use askama::Template;
 
-pub async fn titles(req: actix_web::HttpRequest) -> std::io::Result<HttpResponse> {
+pub async fn titles(req: actix_web::HttpRequest) -> Result<HttpResponse, actix_web::Error> {
     let all = is_all(&req);
-    let obsoleted_map = read_obsoleted_map()?;
-    let title_map = read_title_map()?;
-    let titles = title_map
-        .iter()
-        .map(|(title, page_ids)| TitlesItemTemplate {
-            obsoleted: !page_ids
-                .iter()
-                .any(|page_id| !is_obsoleted(&obsoleted_map, page_id)),
-            title: title.to_string(),
-            url: title_url(&title),
+    let titles =
+        crate::use_case::list_title::list_title(all).map_err(|_| actix_web::Error::from(()))?;
+    let titles = titles
+        .into_iter()
+        .map(|title| TitlesItemTemplate {
+            obsoleted: title.obsoleted,
+            title: title.title.to_string(),
+            url: title_url(&title.title),
         })
         .filter(|template| all || !template.obsoleted)
         .collect::<Vec<TitlesItemTemplate>>();
---
+++ src/use_case/list_title.rs
@@ -0,0 +1,24 @@
+use crate::helpers::{is_obsoleted, read_obsoleted_map, read_title_map};
+use crate::page_title::PageTitle;
+
+#[derive(Clone)]
+pub struct TitleItem {
+  pub title: PageTitle,
+  pub obsoleted: bool,
+}
+
+pub fn list_title(all: bool) -> Result<Vec<TitleItem>, Box<dyn std::error::Error>> {
+  let obsoleted_map = read_obsoleted_map()?;
+  let title_map = read_title_map()?;
+  let titles = title_map
+    .into_iter()
+    .map(|(title, page_ids)| TitleItem {
+      obsoleted: !page_ids
+        .iter()
+        .any(|page_id| !is_obsoleted(&obsoleted_map, page_id)),
+      title,
+    })
+    .filter(|template| all || !template.obsoleted)
+    .collect::<Vec<TitleItem>>();
+  Ok(titles)
+}
