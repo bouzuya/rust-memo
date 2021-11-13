0fe05c9edf64a52143c86a6641113a16a45a736a
bouzuya <m@bouzuya.net>
2021-11-13 19:14:48 +0900

    fix exports in command mod

---
+++ crates/application/src/command.rs
@@ -0,0 +1,18 @@
+mod edit;
+mod insert_links;
+mod link;
+mod list;
+mod list_title;
+mod new;
+mod server;
+mod title;
+
+pub use self::edit::*;
+pub use self::insert_links::*;
+pub use self::link::*;
+pub use self::list::*;
+pub use self::list_title::*;
+pub use self::new::*;
+pub use self::server::*;
+pub use self::title::*;
+pub use self::title::*;
--- crates/application/src/main.rs
+++ crates/application/src/main.rs
@@ -65,17 +65,13 @@ enum Subcommand {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
     match opt.subcommand {
-        Subcommand::Edit { id_like } => crate::command::edit::edit(id_like.as_str())?,
-        Subcommand::InsertLinks { id_like } => {
-            crate::command::insert_links::insert_links(id_like.as_str())?
-        }
-        Subcommand::Link { id_like_or_title } => {
-            crate::command::link::link(id_like_or_title.as_str())?
-        }
-        Subcommand::List { obsoleted } => crate::command::list::list(obsoleted)?,
-        Subcommand::ListTitle { obsoleted } => crate::command::list_title::list_title(obsoleted)?,
-        Subcommand::New { title } => crate::command::new::new(title.as_deref())?,
-        Subcommand::Server => crate::command::server::server()?,
+        Subcommand::Edit { id_like } => crate::command::edit(id_like.as_str())?,
+        Subcommand::InsertLinks { id_like } => crate::command::insert_links(id_like.as_str())?,
+        Subcommand::Link { id_like_or_title } => crate::command::link(id_like_or_title.as_str())?,
+        Subcommand::List { obsoleted } => crate::command::list(obsoleted)?,
+        Subcommand::ListTitle { obsoleted } => crate::command::list_title(obsoleted)?,
+        Subcommand::New { title } => crate::command::new(title.as_deref())?,
+        Subcommand::Server => crate::command::server()?,
         Subcommand::Title { id_like } => crate::command::title(id_like.as_str())?,
     }
     Ok(())
