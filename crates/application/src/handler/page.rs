98655bf011e5488ef321906aa218c15e08d4e39b
bouzuya <m@bouzuya.net>
2021-11-19 21:06:23 +0900

    inline helpers::read_title in page handler

--- crates/application/src/handler/page.rs
+++ crates/application/src/handler/page.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use crate::handler_helpers::is_all;
-use crate::helpers::{is_obsoleted, read_linked_map, read_obsoleted_map, read_title};
+use crate::helpers::{is_obsoleted, read_linked_map, read_obsoleted_map};
 use crate::template::{PageItemTemplate, PageTemplate, PageWithTitle};
 use crate::url_helpers::{page_url, title_url};
 use actix_web::{web, HttpResponse, ResponseError};
@@ -26,21 +26,32 @@ pub async fn page<T: HasPageRepository>(
     let params: (String,) = req.match_info().load().unwrap();
     let page_id = PageId::from_str(&params.0)
         .map_err(|_| std::io::Error::new(std::io::ErrorKind::NotFound, "invalid page_id format"))?;
-    let title = read_title(&page_id);
+    let title = app
+        .page_repository()
+        .find_title(&page_id)
+        .map_err(|_| MyError(format!("IO Error: {}", page_id)))?
+        .ok_or_else(|| MyError(format!("page_id not found: {}", page_id)))?;
     let linked_map = read_linked_map()?;
     let obsoleted_map = read_obsoleted_map()?;
     let linked_by = linked_map
         .get(&title)
         .unwrap_or(&std::collections::BTreeSet::new())
         .iter()
-        .map(|page_id| PageWithTitle {
-            id: page_id.to_string(),
-            obsoleted: is_obsoleted(&obsoleted_map, page_id),
-            title: read_title(page_id).to_string(),
-            url: page_url(page_id),
+        .filter(|&page_id| all || !is_obsoleted(&obsoleted_map, page_id))
+        .map(|page_id| {
+            let title = app
+                .page_repository()
+                .find_title(page_id)
+                .map_err(|_| MyError(format!("IO Error: {}", page_id)))?
+                .ok_or_else(|| MyError(format!("page_id not found: {}", page_id)))?;
+            Ok(PageWithTitle {
+                id: page_id.to_string(),
+                obsoleted: is_obsoleted(&obsoleted_map, page_id),
+                title: title.to_string(),
+                url: page_url(page_id),
+            })
         })
-        .filter(|template| all || !template.obsoleted)
-        .collect::<Vec<PageWithTitle>>();
+        .collect::<actix_web::error::Result<Vec<PageWithTitle>>>()?;
     let obsoleted_by = obsoleted_map
         .get(&page_id)
         .unwrap_or(&std::collections::BTreeSet::new())
