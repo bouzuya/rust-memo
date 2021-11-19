5be00cb034e077ad6d0acdd8c7672c0281c83ea1
bouzuya <m@bouzuya.net>
2021-11-20 08:32:25 +0900

    add PageContent::replace_obsoletes

--- crates/entity/src/entity/page_content.rs
+++ crates/entity/src/entity/page_content.rs
@@ -1,5 +1,20 @@
+use crate::{PageId, PagePath};
+
 #[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct PageContent(String);
+
+impl PageContent {
+    pub fn replace_obsoletes(&mut self, page_id: PageId) {
+        if let Some(index) = self.0.find("\n## Obsoletes") {
+            self.0.truncate(index);
+        }
+        self.0.push_str(&format!(
+            "\n## Obsoletes\n\n- [{}]({})\n",
+            page_id,
+            PagePath::from(page_id),
+        ));
+    }
+}
 
 impl std::fmt::Display for PageContent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -21,6 +36,8 @@ impl From<PageContent> for String {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
@@ -34,6 +51,44 @@ mod tests {
             PageContent::from("content1".to_string()).to_string(),
             "content1"
         );
+    }
+
+    #[test]
+    fn replace_obsoletes_test() -> anyhow::Result<()> {
+        let mut page_content = PageContent::from(vec!["# title1", "", "content1", ""].join("\n"));
+        let page_id = PageId::from_str("20210203T040506Z")?;
+        page_content.replace_obsoletes(page_id);
+        assert_eq!(
+            page_content.to_string(),
+            vec![
+                "# title1",
+                "",
+                "content1",
+                "",
+                "## Obsoletes",
+                "",
+                "- [20210203T040506Z](/pages/20210203T040506Z)",
+                "",
+            ]
+            .join("\n"),
+        );
+        let page_id = PageId::from_str("20210203T040507Z")?;
+        page_content.replace_obsoletes(page_id);
+        assert_eq!(
+            page_content.to_string(),
+            vec![
+                "# title1",
+                "",
+                "content1",
+                "",
+                "## Obsoletes",
+                "",
+                "- [20210203T040507Z](/pages/20210203T040507Z)",
+                "",
+            ]
+            .join("\n"),
+        );
+        Ok(())
     }
 
     #[test]
