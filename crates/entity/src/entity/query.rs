19611ff581ffb8f6d2b97b36f4093f7358f22184
bouzuya <m@bouzuya.net>
2021-11-25 23:21:51 +0900

    add Query::matches

--- crates/entity/src/entity/query.rs
+++ crates/entity/src/entity/query.rs
@@ -7,6 +7,18 @@ pub struct ParseQueryError;
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Query(String);
 
+impl Query {
+    pub fn matches(&self, content: &str) -> Vec<(usize, usize)> {
+        let mut matches = vec![];
+        for (line, line_content) in content.lines().enumerate() {
+            if let Some(col) = line_content.find(self.0.as_str()) {
+                matches.push((line + 1, col + 1));
+            }
+        }
+        matches
+    }
+}
+
 impl std::fmt::Display for Query {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
@@ -26,6 +38,21 @@ mod tests {
     use std::str::FromStr;
 
     use super::*;
+
+    #[test]
+    fn matches_test() -> anyhow::Result<()> {
+        let query = Query::from_str("a")?;
+        assert!(query.matches("").is_empty());
+        assert_eq!(query.matches("a"), vec![(1, 1)]);
+        assert_eq!(query.matches("ba"), vec![(1, 2)]);
+        assert_eq!(query.matches("aba"), vec![(1, 1)]);
+        assert_eq!(query.matches("\n"), vec![]);
+        assert_eq!(query.matches("a\n"), vec![(1, 1)]);
+        assert_eq!(query.matches("a\na"), vec![(1, 1), (2, 1)]);
+        assert_eq!(query.matches("a\nba"), vec![(1, 1), (2, 2)]);
+        assert_eq!(query.matches("\na"), vec![(2, 1)]);
+        Ok(())
+    }
 
     #[test]
     fn str_conversion_test() -> anyhow::Result<()> {
