dfb8467ea2c41c59a3fb67b2369300878a21a607
bouzuya <m@bouzuya.net>
2021-11-28 23:39:23 +0900

    add PageRepository::find_by_query

--- crates/use_case/src/port/page_repository.rs
+++ crates/use_case/src/port/page_repository.rs
@@ -1,9 +1,28 @@
-use entity::{Page, PageContent, PageGraph, PageId};
+use entity::{Page, PageContent, PageGraph, PageId, Query};
 #[cfg(test)]
 use mockall::automock;
 
 #[cfg_attr(test, automock)]
 pub trait PageRepository {
+    // TODO: add tests
+    // TODO: add LineNumber
+    // TODO: add ColumnNumber
+    fn find_by_query(&self, query: &Query) -> anyhow::Result<Vec<(PageId, usize, usize)>> {
+        let mut res = vec![];
+        for page_id in self.find_ids()? {
+            let page_content = self.find_content(&page_id)?;
+            if let Some(page_content) = page_content {
+                res.extend(
+                    query
+                        .matches(String::from(page_content).as_str())
+                        .into_iter()
+                        .map(|(l, c)| (page_id, l, c)),
+                );
+            }
+        }
+        Ok(res)
+    }
+
     fn find_content(&self, page_id: &PageId) -> anyhow::Result<Option<PageContent>>;
 
     fn find_ids(&self) -> anyhow::Result<Vec<PageId>>;
