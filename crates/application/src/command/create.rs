e3b4bf5e75da274fe92c2a356bef8d5d0dc7ea3f
bouzuya <m@bouzuya.net>
2021-12-21 22:40:44 +0900

    Fixed to use server in create command

--- crates/application/src/command/create.rs
+++ crates/application/src/command/create.rs
@@ -1,16 +1,52 @@
-use entity::PageTitle;
-use use_case::{HasNewPageUseCase, NewPageUseCase};
+use std::collections::HashMap;
 
-use crate::helpers::to_file_name;
+use use_case::HasNewPageUseCase;
 
-pub fn create<App: HasNewPageUseCase>(app: App, title: Option<&str>) -> anyhow::Result<()> {
-    let title = PageTitle::from(title.unwrap_or_default().to_string());
-    let (new_page_id, already_exists) = app.new_page_use_case().new_page(title.clone())?;
-    // TODO: use presenter
-    let new_file_name = to_file_name(&new_page_id);
-    println!("{}", new_file_name);
-    if already_exists {
-        println!("{} already_exists", title)
+#[derive(serde::Serialize)]
+struct Body {
+    content: String,
+}
+
+pub fn create<App: HasNewPageUseCase>(_app: App, title: Option<&str>) -> anyhow::Result<()> {
+    let content = title.map(|s| format!("# {}", s)).unwrap_or_default();
+    let client = reqwest::blocking::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()?;
+    let mut form = HashMap::new();
+    form.insert("content", content);
+    let response = client
+        .post("http://localhost:3000/pages")
+        .form(&form)
+        .send()?;
+    let created = if response.status().is_redirection() {
+        if let Some(location) = response.headers().get(reqwest::header::LOCATION) {
+            match location.to_str() {
+                Err(_) => Err(anyhow::anyhow!(
+                    "location header to_str failed : {:?}",
+                    location
+                )),
+                Ok(s) => match s.strip_prefix("/pages/") {
+                    Some(s) => Ok(format!("{}.md", s)),
+                    None => Err(anyhow::anyhow!(
+                        "location header does not start with /pages/ : {}",
+                        s
+                    )),
+                },
+            }
+        } else {
+            Err(anyhow::anyhow!(
+                "location header is not found : {:?}",
+                response.headers()
+            ))
+        }
+    } else {
+        Err(anyhow::anyhow!(
+            "status is not redirection : {}",
+            response.status()
+        ))
+    };
+    if let Ok(created) = created {
+        println!("{}", created);
     }
     Ok(())
 }
